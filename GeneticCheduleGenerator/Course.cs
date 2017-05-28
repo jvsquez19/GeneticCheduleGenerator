1575fe1fd6480ae4e122cdc04b49a3d1e050cfe0
jvsquez19 <jvsquez19@gmail.com>
2017-05-27 20:05:06 -0600

    Some methods added

--- GeneticCheduleGenerator/Course.cs
+++ GeneticCheduleGenerator/Course.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿﻿using System;
 namespace GeneticCheduleGenerator
 {
     public class Course
     {
-        private string name;
-        private int group;
-        private int idCourse;
-        private int idProfessor;
+        public string name{ get; set; }
+        public int semester; //{ get; set; }
+        public int idCourse; //{ get; set; }
+        public int idProfessor; //{ get; set; }
 
         public Course(string n, int idC, int idP, int gr)
         {
             this.name = n;
             this.idCourse = idC;
             this.idProfessor = idP;
-            this.group = gr;
+            this.semester = gr;
         }
     }
 }
--- GeneticCheduleGenerator/Schedule.cs
+++ GeneticCheduleGenerator/Schedule.cs
@@ -1,11 +1,97 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+
 namespace GeneticCheduleGenerator
 {
     public class Schedule
     {
-        // no le veo la funcionalidad
-        private string name { get; set; }
-        private int idProfessor { get; set; }
-        private int assignedHours { get; set; }
+        static int aulas = 10;
+
+        private Course[,,] matrix = new Course[5,4,aulas];
+
+
+        // Generates a one dimension list from the matrix 
+
+        public List<Course>  matrixToList()
+        {
+            List<Course> response = new List<Course>();
+
+            for (int i = 0; i < 5; i++)
+            {
+				for (int j = 0; j < 4; j++)
+				{
+					for (int k = 0; k < aulas; k++)
+					{
+                        response.Add(matrix[i,j,k]);
+
+					}
+				}
+			}
+            return response;
+        }
+
+
+        // converts a received matrix into a list
+        public void listToMatrix(List<Course>list)
+        {
+            int count = 0;
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					for (int k = 0; k < aulas; k++)
+					{
+                        matrix[i, j, k] = list[count];
+                        count++;
+
+					}
+				}
+			}
+        }
+
+        // Returns true if a professor already has to give classes at determinated hour
+
+        public bool IsProfessorCrushes(int idProfesor, int day, int hour)
+        {
+            for (int i = 0; i < aulas; i++)
+            {
+                Course actual = matrix[day, hour, i];
+
+                if(actual != null)
+                {
+                    if(actual.idProfessor == idProfesor)
+                    {
+                        return true; 
+                    }   
+                }
+            }
+            return false;   
+
+        }
+
+		public bool IsSemesterCrushes(int semester, int day, int hour)
+		{
+			for (int i = 0; i < aulas; i++)
+			{
+				Course actual = matrix[day, hour, i];
+
+				if (actual != null)
+				{
+                    if (actual.semester == semester)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+
+		}
+
+
+
+
+
     }
+
+
 }
